4f042e64e3155abfc4b86f61623a4d999dad0f89
Amine Sehili <amine.sehili@gmail.com>
2014-12-24 14:49:48 +0100

    Move tinyHttpServer.py

---
+++ tinyHttpServer.py
@@ -0,0 +1,15 @@
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8080
+
+try:
+   Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+   httpd = SocketServer.TCPServer(("", PORT), Handler)
+   print("serving at port %d" % (PORT))
+   print("Type Ctrl+C to quit")
+   httpd.serve_forever()
+
+except KeyboardInterrupt as e:
+  print("\nserver stopped\nBye...") 
+
